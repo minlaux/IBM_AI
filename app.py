8b01aa68c0f228e9637fd8b9fda2f85e308336d4
Polina <93955709+minlaux@users.noreply.github.com>
2023-08-09 14:51:04 -0400

    sample error responses

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(message = "Hello world!") #{"message": "Hello world!"}
+    return "hello world"
+
+@app.route('/no_content')
+def no_content():
+    return ({"message":"No content found"}, 204)
+
+@app.route('/exp')
+def index_explicit():
+    """
+    give response "Hello World!" when status 200
+    """
+    resp = make_response({"message": "Hello World!"})
+    resp.status_code = 200
+    return resp
