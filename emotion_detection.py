523cb8e334f992187650ca36e022a525173117e0
Polina <93955709+minlaux@users.noreply.github.com>
2023-08-12 17:12:42 -0400

    created connection to Watson NLP library

---
+++ emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_data = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=input_data)
+
+    if response.status_code == 200:
+        result = response.json()
+        return result['text']
+    else:
+        return f"Emotion detection request failed with status code: {response.status_code}"
+
+# test
+if __name__ == "__main__":
+    text = "I am feeling excited about the final project!"
+    result_text = emotion_detector(text)
+    print("Emotion detected:", result_text)
